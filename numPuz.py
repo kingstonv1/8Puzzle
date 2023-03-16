37de65de2f1d236831861cd818b8fc54472f3636
kingstonv1 <kingstongise@gmail.com>
2023-03-16 12:46:43 -0400

    Puzzle is never generated impossible!

--- numPuz.py
+++ numPuz.py
@@ -1,18 +1,7 @@
 import pygame
 import sys
 import random
-
-
-def generate_tiles():
-	possibleTiles = [1, 2, 3, 4, 5, 6, 7, 8, ' ']
-	res = [[], [], []]
-	
-	for x in range(3):
-		for y in range(3):
-			res[x].append(Tile(random.choice(possibleTiles), x, y))
-			possibleTiles.remove(res[x][y].value)
-	
-	return res
+import math
 
 
 class Tile:
@@ -47,7 +36,7 @@ class Tile:
 			self.position = (self.position[0], self.position[1] - 1)
 			self.reposition()
 		elif direction == 'DOWN':
-			self.position = (self.position[0], self.position[1] +1)
+			self.position = (self.position[0], self.position[1] + 1)
 			self.reposition()
 		elif direction == 'LEFT':
 			self.position = (self.position[0] - 1, self.position[1])
@@ -63,22 +52,14 @@ class Game:
 		self.tiles = generate_tiles()
 		
 		# While loop that makes sure the puzzle is never generated pre-solved.
-		while self.is_solved():
+		while self.count_inversions() % 2 == 1:
 			self.tiles = generate_tiles()
 	
 	def get_tile_values(self):
-		res = [[], [], []]
-		
-		for x in range(3):
-			for y in range(3):
-				res[x].append(self.tiles[x][y].value)
-		
-		return res
+		return [[self.tiles[x][y].value for x in range(3)] for y in range(3)]
 	
 	def is_solved(self):
-		if self.get_tile_values() == [[1, 4, 7], [2, 5, 8], [3, 6, ' ']]:
-			return True
-		return False
+		return True if self.get_tile_values() == [[1, 2, 3], [4, 5, 6], [7, 8, ' ']] else False
 	
 	def draw_tiles(self, surface):
 		for x in range(3):
@@ -101,8 +82,10 @@ class Game:
 					self.tiles[x][y].move(direction)
 					return
 	
+	# noinspection PyUnresolvedReferences
 	def move_tile(self, tileX, tileY):
 		clickedTile = self.tiles[tileX][tileY]
+		
 		if tileY > 0:
 			if self.tiles[tileX][tileY - 1].value == ' ':
 				clickedTile.move('UP')
@@ -130,21 +113,44 @@ class Game:
 				self.move_blank_tile('LEFT')
 				self.tiles[tileX + 1][tileY], self.tiles[tileX][tileY] = \
 					self.tiles[tileX][tileY], self.tiles[tileX + 1][tileY]
+				
+	def count_inversions(self):
+		inversions = 0
+		
+		for x in range(9):
+			selectedVal = self.tiles[math.floor(x / 3)][x % 3].value
+			if selectedVal == ' ':
+				continue
+			
+			for y in range(x, 9):
+				comparedVal = self.tiles[math.floor(y / 3)][y % 3].value
+				if comparedVal == ' ':
+					continue
+				inversions += 1 if selectedVal > comparedVal else 0
+		
+		return inversions + 1
+	
+
+def generate_tiles():
+	possibleTiles = [1, 2, 3, 4, 5, 6, 7, 8, ' ']
+	res = [[], [], []]
+	
+	for x in range(3):
+		for y in range(3):
+			res[x].append(Tile(random.choice(possibleTiles), x, y))
+			possibleTiles.remove(res[x][y].value)
+	
+	return res
 
 
 def draw_grid(surface):
-	global width, height
-	
 	black = (0, 0, 0)
-	
 	# Draw outline along window lines
 	boardOutlinePoints = ((0, 0), (width, 0), (width, height), (0, height))
 	pygame.draw.lines(surface, black, closed = True, points = boardOutlinePoints, width = 2)
-	
 	# Draw horizontal grid lines
 	for i in range(1, 3):
 		pygame.draw.line(surface, black, start_pos = (0, i * 200), end_pos = (width, i * 200))
-	
 	# Draw vertical grid lines
 	for i in range(1, 3):
 		pygame.draw.line(surface, black, start_pos = (i * 200, 0), end_pos = (i * 200, height))
@@ -160,7 +166,7 @@ screen = pygame.display.set_mode((width, height))
 puzzle = Game()
 
 pygame.display.set_caption('Sliding Number Puzzle')
-textGenerator = pygame.font.SysFont('georgia', 48)
+textGenerator = pygame.font.SysFont('arial', 48)
 pygame.mouse.set_visible(True)
 
 while True:
